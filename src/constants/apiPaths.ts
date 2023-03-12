2fcebb741274a91f380551941fd39b9e396caea4
tanyak1601 <tanyakk1601@gmail.com>
2023-03-12 15:54:22 +0300

    add task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://q5ah4xgy4k.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   products: "https://82or31sgi6.execute-api.eu-west-1.amazonaws.com/dev",
