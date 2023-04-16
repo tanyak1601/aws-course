0e83f3e7080e5f4bcfd9377228d658d82807bcf2
tanyak1601 <tanyakk1601@gmail.com>
2023-04-16 17:38:01 +0300

    update cart API endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://q5ah4xgy4k.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://tanyak1601-cart-api-dev.eu-west-1.elasticbeanstalk.com/",
   products: "https://82csco7pl8.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
